f0b44c25ca01d771c1bbe6626e82b28d6b4c4af2
IsaacOc <ochomisaac356@gmail.com>
2019-10-24 16:19:35 +0300

    [ch11] add smtp email service to notification services

--- service/client.go
+++ service/client.go
@@ -1,113 +1,140 @@
 package service
 
-import(
+import (
+	"fmt"
+	"io"
+	"log"
+
 	"bitbucket.org/antinvestor/service-notification/notification"
 	"google.golang.org/grpc"
-	"fmt"
-	"log"
+
 	//"github.com/rs/xid"
 	"context"
 	"time"
+
 	"github.com/jinzhu/gorm"
 )
-	
+
 //Runclient runs client server
-func Runclient(db *gorm.DB){
+func Runclient(db *gorm.DB) {
 
-
-	
 	fmt.Println("hello I'm a client")
-	cc, err := grpc.Dial("0.0.0.0:7000",grpc.WithInsecure())
-if err != nil{
-	log.Fatalf("could not connect : %v" ,err)
+	cc, err := grpc.Dial("0.0.0.0:7000", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("could not connect : %v", err)
+	}
+	defer cc.Close()
+
+	c := notification.NewNotificationServiceClient(cc)
+
+	//client methods called on particular occasion/functionality
+
+	//search(c)
+	//status(c)
+	//dosend(c)
+	income(c)
+
 }
-defer cc.Close()
-
-c := notification.NewNotificationServiceClient(cc)
-
-
-//client methods called on particular occasion/functionality
-
-status(c)
-//dosend(c)
-//income(c)
-
-		}
 
 // income notification requests
-func income(c notification.NotificationServiceClient){
+func income(c notification.NotificationServiceClient) {
 
-			req :=&notification.IncomeRequest{
-				Requeststatus:		"send",//req.Requeststatus,
-				Language:			"English",//req.Language,
-				Product:			"Funds",//req.Product,
-				Massagetype:		"Recieved",//req.Massagetype,
-				ProfileID :			map[string] string{
-											"name":"isa",
-				 
-									},
-
-}
-env2, cancel := context.WithTimeout(context.Background(), time.Second * 15)
+	req := &notification.IncomeRequest{
+		RequestStatus: "send",     //req.Requeststatus,
+		Language:      "English",  //req.Language,
+		ProductID:     "Funds",    //req.Product,
+		MessageType:   "Recieved", //req.Massagetype,
+		ProfileID:     "001isaac",
+		PayLoad: map[string]string{
+			"id":         "1",
+			"name":        "test entity 1",
+			"description": "Testing email service on the notification service part",
+		},
+	}
+	env2, cancel := context.WithTimeout(context.Background(), time.Second*15)
 	defer cancel()
-res ,err := c.In(env2,req)
+	res, err := c.In(env2, req)
 
-if err != nil {
-	log.Fatalf("error while call send RPC %v",err)
+	if err != nil {
+		log.Fatalf("error while call send RPC %v", err)
+	}
+	log.Printf("Response from sender: %s", res.GetNotificationID())
+		
+	
+	//dosend creates notification for outgoing
+		dosend(c)
 }
-log.Printf("Response from sender: %s",res.GetNotificationID())
-}
-
 
 //dosend creates notification for outgoing
-func dosend(c notification.NotificationServiceClient){
+func dosend(c notification.NotificationServiceClient) {
 
-	req :=&notification.QueueRequest{
-			
-			
-			Language:					"English",
-			Channel:					"Email",
-			Massagetemplete:			"Receveid_templete",
-			Autosend:	    			"false",
-			ProfileID :			map[string] string{
-									"name":"isa",
-		 
-							},
-			Massagevariables:			map[string] string{
-								"name":"isa",
-								"Account":"AC100000",
-								"Amount": "100000",
-	 
-						},
+	req := &notification.QueueRequest{
 
+		Language:        "English",
+		Channel:         "Email",
+		MessageTemplete: "Receveid_templete",
+		Autosend:        "false",
+		ProfileID:       "001isaac",
+		MessageVariables: map[string]string{
+			"name":    "isa",
+			"Account": "AC100000",
+			"Amount":  "100000",
+		},
+		
+	}
+	env2, cancel := context.WithTimeout(context.Background(), time.Second*15)
+	defer cancel()
+	res, err := c.Out(env2, req)
 
-}
-env2, cancel := context.WithTimeout(context.Background(), time.Second * 15)
-defer cancel()
-res ,err := c.Out(env2,req)
-
-if err != nil {
-log.Fatalf("error while call send RPC %v",err)
-}
-log.Printf("Response from sender: %s",res.GetNotificationID())
+	if err != nil {
+		log.Fatalf("error while call send RPC %v", err)
+	}
+	log.Printf("Response from sender: %s", res.GetNotificationID())
 }
 
 //status looks for notification status with notification id
-func status(c notification.NotificationServiceClient){
+func status(c notification.NotificationServiceClient) {
 
-	req :=&notification.StatusRequest{
-			
-			
-			NotificationID:					"bmg4apdq29bieu2s9640",
-			
+	req := &notification.StatusRequest{
+
+		NotificationID: "bmg4apdq29bieu2s9640",
+	}
+
+	env2, cancel := context.WithTimeout(context.Background(), time.Second*15)
+	defer cancel()
+	res, err := c.Status(env2, req)
+
+	if err != nil {
+		log.Fatalf("error while call send RPC %v", err)
+	}
+	log.Printf("Response from sender: %s", res.GetMessageStatus())
 }
 
-env2, cancel := context.WithTimeout(context.Background(), time.Second * 15)
-defer cancel()
-res ,err := c.Status(env2,req)
+func search(c notification.NotificationServiceClient) {
 
-if err != nil {
-log.Fatalf("error while call send RPC %v",err)
+	req := &notification.SearchRequest{
+
+		NotificationID: "bmg4apdq29bieu2s9640",
+	}
+
+	env2, cancel := context.WithTimeout(context.Background(), time.Second*15)
+	defer cancel()
+	stream, err := c.Search(env2, req)
+
+	if err != nil {
+		log.Fatalf("%v.Search(_) = _, %v", c, err)
+	}
+
+	for {
+		id, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("%v.Search(_) = _, %v", c, err)
+		}
+		log.Printf("Response from sender: %s", id)
+	}
+
 }
-log.Printf("Response from sender: %s",res.GetMessagestatus())
-}
+
--- service/handler.go
+++ service/handler.go
@@ -3,79 +3,350 @@ package service
 import (
 	"context"
 	"encoding/json"
-	//"time"
+	"errors"
+	"log"
+	"time"
+
 	"bitbucket.org/antinvestor/service-notification/notification"
-	//"fmt"
-	//"github.com/jinzhu/gorm"
-	//"log"
+
 	"github.com/rs/xid"
+
+	stan "github.com/nats-io/stan.go"
+
+	"net/smtp"
 )
 
 
 
 type notificationserver struct {
-	Env *Env
+	Env    *Env
+	stream *notification.NotificationService_SearchServer
 }
 
-func (server *notificationserver) Out(ctxt context.Context, req *notification.QueueRequest) (*notification.QueueResponse, error){
-	
-	uProfile, _ := json.Marshal(req.GetProfileID())
-	Massagevariables, _ := json.Marshal(req.GetMassagevariables())
-	
-	in := &Notification{
-			NotificationID:				xid.New().String(),
-			ProfileID : 				string(uProfile),
-			Messagevariables:			string(Massagevariables),
-			Language:					req.Language,
-			Channel:					req.Channel,
-			Messagetype:				req.Massagetemplete,
-			Autosend:	    			req.Autosend,
-			}
+//out method act after income request let out notification
+func (server *notificationserver) Out(ctxt context.Context, req *notification.QueueRequest) (*notification.StatusResponse, error) {
 
-			server.Env.GeWtDb(ctxt).Create(in)
+	Massagevariables, _ := json.Marshal(req.GetMessageVariables())
 
-	return &notification.QueueResponse{NotificationID: xid.New().String(),}, nil
+	NOTID := xid.New().String()
+	var id []string
 
-}	
+	//checks if profileID is valid
+	server.Env.GeWtDb(ctxt).Where("profile_id = ?", req.GetProfileID()).Find(&Notification{}).Pluck("profile_id", &id)
+	if len(id) != 0 {
+
+		
+		//send notification
+		in := &Notification{
+			NotificationID:   NOTID,
+			ProfileID:        req.GetProfileID(),
+			Messagevariables: string(Massagevariables),
+			Language:         req.Language,
+			//channel will be determined by product preferences or profile client request
+			//if the user has multiple phone numbers attached to their profile prefer the phone number that was last added and verified or last known to work
+			Channel:     req.Channel,
+			Messagetype: req.MessageTemplete,
+			Autosend:    req.Autosend,
+			ProductID:"3900Rent",
+			Payload: string(Massagevariables),
+		}
+
+		server.Env.GeWtDb(ctxt).Create(in)
+		
+		//nats stream subscribation
+		go subscribetion()
+		go QueueSubscribeGroup()
+		go QueueSubscribeGroup2()
+
+		
+	} else {
+		return nil, errors.New("ProfileID doesnot exit/match any value")
+
+	}
+
+	return &notification.StatusResponse{NotificationID: NOTID}, nil
+
+}
 
 //Status
-func (server *notificationserver) Status(ctxt context.Context, req *notification.StatusRequest) (*notification.StatusResponse, error){
+func (server *notificationserver) Status(ctxt context.Context, req *notification.StatusRequest) (*notification.StatusResponse, error) {
 
-	var StatusInfos [] string
+	var StatusInfos []string
 	var status string
 
 	//server.Env.GeWtDb(ctxt).Debug().Raw("select status from notifications where notification_id = ?", req.GetNotificationID()).Pluck("status", &StatusInfos)
 	server.Env.GeWtDb(ctxt).Debug().Table("notifications").Select("status").Where("notification_id = ?", req.GetNotificationID()).Pluck("status", &StatusInfos)
+	if len(StatusInfos) != 0 {
 
-	for _,status := range StatusInfos {
-	
-	return &notification.StatusResponse{Messagestatus: status,}, nil
+		for _, status := range StatusInfos {
+
+			return &notification.StatusResponse{MessageStatus: status}, nil
+		}
+
+	} else {
+
+		return nil, errors.New("Invalid status request")
+	}
+
+	return &notification.StatusResponse{MessageStatus: status}, nil
+
 }
 
-return &notification.StatusResponse{Messagestatus: status,}, nil
+//Release method that is called for messages queued for release
+func (server *notificationserver) Release(ctxt context.Context, req *notification.ReleaseRequest) (*notification.StatusResponse, error) {
 
+	var Notificationid []string
+	var notifications string
+
+	server.Env.GeWtDb(ctxt).Debug().Table("notifications").Select("notification_id").Where("status = ?", req.GetReleaseMessage()).Pluck("notification_id", &Notificationid)
+	if len(Notificationid) != 0 {
+
+		for _, notifications := range Notificationid {
+
+			return &notification.StatusResponse{NotificationID: notifications}, nil
+		}
+	} else {
+
+		return nil, errors.New("Invalid status request")
+	}
+
+	return &notification.StatusResponse{NotificationID: notifications}, nil
 }
 
-func (server *notificationserver) Release(ctxt context.Context, req *notification.ReleaseRequest) (*notification.StatusResponse, error){
+//In method call for income rquest of any notification
+func (server *notificationserver) In(ctxt context.Context, req *notification.IncomeRequest) (*notification.StatusResponse, error) {
+	NOTID := xid.New().String()
 
-	return &notification.StatusResponse{}, nil
-}
+	//checks if profileID  contact field is not null in table
+	if len(req.GetProfileID()) == 0 || req.ProductID == ""{
 
-func (server *notificationserver) In(ctxt context.Context, req *notification.IncomeRequest) (*notification.QueueResponse, error){
-	
-	uProfile, _ := json.Marshal(req.GetProfileID())
-	
-	in := &Notification{
-			NotificationID: xid.New().String(),
-			ProfileID : 	string(uProfile),
-			Status:			req.Requeststatus,
-			Language:		req.Language,
-			Product:		req.Product,
-			Messagetype:	req.Massagetype,
-			}
-
-			server.Env.GeWtDb(ctxt).Create(in)
 			
-	return &notification.QueueResponse{NotificationID: xid.New().String(),}, nil
+			return nil, errors.New("ProfileID or ProductID is invalid")
 
+	} 
+
+		uPayload, _ := json.Marshal(req.GetPayLoad())
+		
+		in := &Notification{
+			NotificationID: NOTID,
+			ProfileID:      req.GetProfileID(),
+			ProductID:      req.ProductID,
+			Status:         req.RequestStatus,
+			Language:       req.Language,
+			Messagetype:    req.MessageType,
+			Payload:        string(uPayload),
+			Channel:		"Email",
+		}
+			//create notification
+			server.Env.GeWtDb(ctxt).Create(in)
+
+			go publishEvent(in)
+		 
+
+	 
+
+	return &notification.StatusResponse{NotificationID: NOTID}, nil
+
+}
+
+func (server *notificationserver) Search(req *notification.SearchRequest, stream notification.NotificationService_SearchServer) error {
+
+	var cxt context.Context
+	var serch []string
+
+	server.Env.GeWtDb(cxt).Where("notification_id = ?", req.GetNotificationID()).Find(&Notification{}).Pluck("status", &serch)
+	//check if request input exist in database
+	if len(serch) != 0 {
+
+		for _, d := range serch {
+
+			if err := stream.Send(&notification.SearchResponse{RequestStatus: d}); err != nil {
+				return err
+			}
+		}
+
+	} else {
+		return errors.New("notificationID is invalid")
+
+	}
+	return nil
+
+}
+
+// publishEvent publish an event via NATS Streaming server
+func publishEvent(model *Notification) error {
+
+	const (
+		clusterID = "test-cluster"
+		clientID  = "event-store"
+	)
+
+	// Connect to NATS Streaming server
+	sc, err := stan.Connect(
+		clusterID,
+		clientID,
+		stan.NatsURL(stan.DefaultNatsURL),
+	)
+	if err != nil {
+		return err
+	}
+	defer sc.Close()
+	channel := model.Channel
+	eventMsg := []byte(model.Payload)
+	// Publish message on subject (channel)
+	//for i:=0;i<=5;i++{
+	sc.Publish(channel, eventMsg)
+	//}
+	log.Println("Published message on channel: " + channel)
+
+	return nil
+}
+
+//subscribetion event
+func subscribetion() {
+
+	const (
+		clusterID = "test-cluster"
+		clientID  = "notification-service"
+		channel   = "Email"
+		durableID = "notification-service-durable"
+	)
+
+	sc, err := stan.Connect(
+		clusterID,
+		clientID,
+		stan.NatsURL(stan.DefaultNatsURL),
+	)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Subscribe with manual ack mode, and set AckWait to 60 seconds
+	aw, _ := time.ParseDuration("60s")
+	sc.Subscribe(channel, func(msg *stan.Msg) {
+		msg.Ack() // Manual ACK
+		order := &Notification{}
+		//Unmarshal JSON that represents the Order data
+		err := json.Unmarshal(msg.Data, &order)
+		if err != nil {
+			log.Print("ll",err)
+			return
+		}
+		//Handle the message
+		log.Printf("Subscribed message from clientID - %s for Order: %+v\n", clientID,string(msg.Data))
+		
+		HandleEmail(msg.Data)
+
+	}, stan.DurableName(durableID),
+		stan.MaxInflight(25),
+		stan.SetManualAckMode(),
+		stan.AckWait(aw),
+	)
+}
+//QueueSubscribeGroup duarable queue subscription with same durable Name
+func QueueSubscribeGroup() {
+
+	const (
+		clusterID  = "test-cluster"
+		clientID   = "notification-service-query"
+		channel    = "Email"
+		durableID  = "notification-durable"
+		queueGroup = "notification-service-group"
+	)
+
+	// Connect to NATS Streaming server
+	sc, err := stan.Connect(
+		clusterID,
+		clientID,
+		stan.NatsURL(stan.DefaultNatsURL),
+	)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	sc.QueueSubscribe(channel, queueGroup, func(msg *stan.Msg) {
+		order := &Notification{}
+		err := json.Unmarshal(msg.Data, &order)
+		if err == nil {
+			// Handle the message
+			log.Printf("QueueSubscribed message from clientID - %s: %+v\n", clientID, string(msg.Data))
+			 
+			HandleEmail(msg.Data)
+		}
+	}, stan.DurableName(durableID),
+	)
+	//runtime.Goexit()
+}
+
+//QueueSubscribeGroup2 duarable queue subscription with same durable Name
+func QueueSubscribeGroup2() {
+
+	const (
+		clusterID  = "test-cluster"
+		clientID   = "notification-service-query2"
+		channel    = "Email"
+		durableID  = "notification-durable"
+		queueGroup = "notification-service-group"
+	)
+
+	// Connect to NATS Streaming server
+	sc, err := stan.Connect(
+		clusterID,
+		clientID,
+		stan.NatsURL(stan.DefaultNatsURL),
+	)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	sc.QueueSubscribe(channel, queueGroup, func(msg *stan.Msg) {
+		order := &Notification{}
+		err := json.Unmarshal(msg.Data, &order)
+		if err == nil {
+			// Handle the message
+			log.Printf("2QueueSubscribed message from clientID - %s: %+v\n", clientID, string(msg.Data))
+			
+		}
+	}, stan.DurableName(durableID),
+	)
+	//runtime.Goexit()
+}
+
+// smtpServer data to smtp server
+type smtpServer struct {
+	host string
+	port string
+   }
+   // serverName URI to smtp server
+   func (s *smtpServer) serverName() string {
+	return s.host + ":" + s.port
+   }
+
+
+//HandleEmail Handle Email
+func HandleEmail( msg []byte) {
+    // Sender data.
+    from := "ochomisaac356@gmail.com"
+    password := "jaydeth1"
+    // Receiver email address.
+    to := []string{
+        "info@antinvestor.com",
+        
+    }
+    // smtp server configuration.
+    smtpServer := smtpServer{host: "smtp.gmail.com", port: "587"}
+    // Message.
+	message := []byte("To: info@antinvestor.com \r\n" +
+	"Subject: Notification service \r\n " + 
+	"\r\n" + 
+	 "Welcome\r\n" + string(msg) )
+    // Authentication.
+    auth := smtp.PlainAuth("", from, password, smtpServer.host)
+    // Sending email.
+    err := smtp.SendMail(smtpServer.serverName(), auth, from, to, message)
+    if err != nil {
+        log.Println(err)
+        return
+    }
+    log.Println("Email Sent!")
 }
