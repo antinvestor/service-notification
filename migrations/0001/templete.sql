3d62e3d64de8472d42c1a678f0a7f5fa199366fc
IsaacOc <ochomisaac356@gmail.com>
2019-11-08 15:45:51 +0300

    [ch11] improvement to migration file build

--- migrations/0001/templete.sql
+++ migrations/0001/templete.sql
@@ -1,4 +1,4 @@
 CREATE SEQUENCE templetes_serial MINVALUE 0010000;
 
-INSERT INTO message_templetes(message_templete_id,templete_name,templete_value,language, applied_at,created_at,modified_at,version) VALUES 
-	(nextval('templetes_serial'),'Receveid_templete', 'Username;Account;Amount;','English', now(),now(),now(),0);
+INSERT INTO message_templetes(message_templete_id,templete_name,templete_value,language_id, applied_at,created_at,modified_at,version) VALUES 
+	(nextval('templetes_serial'),'Receveid_templete', 'Username;Account;Amount;','lg_23d5f667hhn', now(),now(),now(),0);
