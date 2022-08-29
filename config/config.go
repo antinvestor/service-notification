3dccb5d90293303f2d4a87c70b1a7838900c61cc
Peter Bwire <bwire517@gmail.com>
2022-08-30 01:08:02 +0300

    upgrade frame version

--- config/config.go
+++ config/config.go
@@ -4,7 +4,6 @@ import "github.com/pitabwire/frame"
 
 type NotificationConfig struct {
 	frame.DefaultConfiguration
-	frame.OAUTH2Configuration
 	frame.DatabaseConfiguration
 	ProfileServiceURI   string `default:"127.0.0.1:7005" envconfig:"PROFILE_SERVICE_URI"`
 	PartitionServiceURI string `default:"127.0.0.1:7003" envconfig:"PARTITION_SERVICE_URI"`
